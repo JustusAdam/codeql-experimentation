c5a3609f2fb536f59d2da6bde4d440be150cdff8
Justus Adam <dev@justus.science>
2024-09-25 16:31:47 -0400

    Very simple cross function flow

--- cpp/simple-flow/main.cpp
+++ cpp/simple-flow/main.cpp
@@ -1,11 +1,22 @@
 
+int source()
+{
+    return 2;
+}
+
+int a_function()
+{
+    return source();
+}
+
+int target(int source)
+{
+    return source;
+}
+
 int main(int argv, char **argc)
 {
-    int g = 0;
-
-    int q = 1;
-    q = g;
-    int f = q;
+    target(a_function());
 
     return 0;
 }
